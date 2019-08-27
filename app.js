6fe2178dfada9a2521269422b22c07c101e44f93
Yintong <zhouyintong96@gmail.com>
2019-08-27 13:01:40 +0200

    add repeat

--- app.js
+++ app.js
@@ -6,16 +6,26 @@ app.get("/", (req, res) => res.send("Hi there! Welcome to my assignment"));
 app.get("/speak/:animal", function (req, res) {
     var sounds = {
         pig: 'Oink',
-        cow:'Moo',
-        dog:'Woof Woof',
-        cat:'I hate you human',
-        goldfish:'...'
+        cow: 'Moo',
+        dog: 'Woof Woof',
+        cat: 'I hate you human',
+        goldfish: '...'
     };
 
     var animal = req.params.animal;
     var sound = sounds[animal];
-    res.send("The " + animal +" says " + sound);
+    res.send("The " + animal + " says " + sound);
 })
+
+app.get("/repeat/:message/:times", function (req, res) {
+    var message = req.params.message;
+    var times = req.params.times;
+    var result = "";
+    for (var i = 0; i < times; i++) {
+        result += message + " ";
+    }
+    res.send(result);
+});
 
 app.get("*", (req, res) => res.send("The page not found....................."))
 app.listen(3000, () => console.log("Server is started!"))
